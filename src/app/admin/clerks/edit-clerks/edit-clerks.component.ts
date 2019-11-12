4ec0120d69691d95da521b75452d27a5277d29b3
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-11-12 14:26:51 +0530

    Completed update clerk

--- src/app/admin/clerks/edit-clerks/edit-clerks.component.ts
+++ src/app/admin/clerks/edit-clerks/edit-clerks.component.ts
@@ -12,27 +12,14 @@ import { Clerk } from "src/app/models/clerk";
 import { ClerkService } from "src/app/services/clerk.service";
 
 @Component({
-  selector: "app-add-clerks",
-  templateUrl: "./add-clerks.component.html",
-  styleUrls: ["./add-clerks.component.css"]
+  selector: "app-edit-clerks",
+  templateUrl: "./edit-clerks.component.html",
+  styleUrls: ["./edit-clerks.component.css"]
 })
-export class AddClerksComponent implements OnInit {
+export class EditClerksComponent implements OnInit {
   clerk: Clerk;
   clerkForm: FormGroup;
-
   selectedFile = null;
-
-  fullnametxt: string = "";
-  nameinitialstxt: string = "";
-  positiontxt: string = "";
-  gendertxt: string = "";
-  dobtxt: string = "";
-  nictxt: string = "";
-  addresstxt: string = "";
-  contacttxt: string = "";
-  emailtxt: string = "";
-  firstadmissiontxt: string = "";
-  scladmissiontxt: string = "";
 
   constructor(
     private fb: FormBuilder,
@@ -58,20 +45,45 @@ export class AddClerksComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.clerkService.currentClerk.subscribe(clerk => (this.clerk = clerk));
+    if (this.clerk) {
+      this.clerkForm.get("fullname").setValue(this.clerk.fullname);
+      this.clerkForm.get("nameinitials").setValue(this.clerk.nameinitials);
+      this.clerkForm.get("position").setValue(this.clerk.position);
+      this.clerkForm.get("gender").setValue(this.clerk.gender);
+      this.clerkForm.get("dob").setValue(this.clerk.dob);
+      this.clerkForm.get("nic").setValue(this.clerk.nic);
+      this.clerkForm.get("address").setValue(this.clerk.address);
+      this.clerkForm.get("contact").setValue(this.clerk.contact);
+      this.clerkForm.get("email").setValue(this.clerk.email);
+      this.clerkForm.get("firstadmission").setValue(this.clerk.firstadmission);
+      this.clerkForm.get("scladmission").setValue(this.clerk.scladmission);
+      this.clerkForm.get("file").setValue(null);
+    }
+  }
+
+  getError(texttype) {
+    return texttype.hasError("required")
+      ? "You must enter a value"
+      : texttype.hasError("email")
+      ? "Not a valid email"
+      : "";
+  }
 
   onFileSelected(event) {
     this.selectedFile = event.target.files[0];
   }
 
-  Save(data) {
+  editClerk(data) {
+    console.log(this.clerk.clerkId);
     if (this.clerkForm.invalid) return;
 
     Swal.showLoading();
 
     data.file = this.selectedFile;
 
-    this.clerkService.registerClerk(data).subscribe(
+    this.clerkService.editClerk(this.clerk.clerkId, data).subscribe(
       data => {
         Swal.hideLoading();
         Swal.fire({
@@ -92,13 +104,5 @@ export class AddClerksComponent implements OnInit {
         });
       }
     );
-  }
-
-  getError(texttype) {
-    return texttype.hasError("required")
-      ? "You must enter a value"
-      : texttype.hasError("email")
-      ? "Not a valid email"
-      : "";
   }
 }
