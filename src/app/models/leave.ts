52436f6a80277a602abe34d6b07d90d09946a7dd
sandalu95 <sandalukalpanee@gmail.com>
2019-11-23 12:57:27 +0530

    Created Teacher leaves feature

---
+++ src/app/models/leave.ts
@@ -0,0 +1,11 @@
+export class Leave {
+    id: string;
+    userId: string;
+    commencedDate: string;
+    assumedDate: string;
+    appliedDate: string;
+    noOfDays: string;
+    leaveType: string;
+    reason:string;
+    assignedWorkId:string;
+}
