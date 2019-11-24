9e4134aa076f5b082436d92b6076fcdcab514ba3
sandalu95 <sandalukalpanee@gmail.com>
2019-11-24 12:38:13 +0530

    Created Student portal

---
+++ src/app/student/student-exams/student-exams.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-exams',
+  templateUrl: './student-exams.component.html',
+  styleUrls: ['./student-exams.component.css']
+})
+export class StudentExamsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
