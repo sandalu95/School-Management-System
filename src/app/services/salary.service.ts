4eea03a314cdd177912be25bd64b800fa8683d02
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-12-16 01:25:50 +0530

    Completed view salary feature

--- src/app/services/salary.service.ts
+++ src/app/services/salary.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Salary } from "../models/salary";
 import { Observable } from "rxjs";
 import { CommonResponse } from "../models/response/commonResponse";
 import { GetUsersResponse } from "../models/response/getUsersResponse";
+import { GetSalaryResponse } from "../models/response/getSalaryResponse";
 
 @Injectable({
   providedIn: "root"
 })
 export class SalaryService {
-  apiURL: string = "https://sms-web-service.herokuapp.com/api/salary";
+  apiURL: string = "http://localhost:3000/api/salary";
   apiURLGetUsers: string =
     "https://sms-web-service.herokuapp.com/api/user/user";
   user = JSON.parse(localStorage.getItem("httpCache"));
@@ -22,9 +23,25 @@ export class SalaryService {
       headers: this.headers
     });
   }
+
   public getUsers(): Observable<GetUsersResponse> {
     return this.http.get<GetUsersResponse>(this.apiURLGetUsers, {
       headers: this.headers
     });
+  }
+
+  public getSalaryByUser(
+    year: string,
+    month: string
+  ): Observable<GetSalaryResponse> {
+    let params = new HttpParams();
+    params = params.append("year", year);
+    params = params.append("month", month);
+
+    const options = {
+      params: params,
+      headers: this.headers
+    };
+    return this.http.get<GetSalaryResponse>(`${this.apiURL}/byuser`, options);
   }
 }
--- src/app/teacher/teacher-salary/teacher-salary.component.ts
+++ src/app/teacher/teacher-salary/teacher-salary.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Pay } from 'src/app/models/pay';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Pay } from "src/app/models/pay";
+import { SalaryService } from "src/app/services/salary.service";
+import Swal from "sweetalert2";
+import { Salary } from "src/app/models/salary";
+import { Teacher } from "src/app/models/teacher";
 
 @Component({
-  selector: 'app-teacher-salary',
-  templateUrl: './teacher-salary.component.html',
-  styleUrls: ['./teacher-salary.component.css']
+  selector: "app-teacher-salary",
+  templateUrl: "./teacher-salary.component.html",
+  styleUrls: ["./teacher-salary.component.css"]
 })
 export class TeacherSalaryComponent implements OnInit {
-
   searchForm: FormGroup;
-  // public salary: Salary;
+
   name: string;
   designation: string;
   firstAppointmentDate: string;
@@ -20,54 +23,172 @@ export class TeacherSalaryComponent implements OnInit {
   earnings: Pay[];
   deductions: Pay[];
 
-  // constructor(private fb: FormBuilder, private salaryService: SalaryService) {}
+  salary: Salary;
+  teacher: Teacher;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private salaryService: SalaryService) {
     this.searchForm = fb.group({
       year: [null, Validators.required],
-      month: [null, Validators.required],
+      month: [null, Validators.required]
     });
   }
 
   ngOnInit() {
-    this.name="drgdg";
-    this.designation= "dfvvf";
-    this.firstAppointmentDate= "2019-08-12";
-    this.schoolAppointmentDate= "2019-03-12";
-    this.retirementDate= "2017-02-09";
-    this.basicSalary= "4353";
-    this.earnings= [
-      {
-        name:"Basic Salary",
-        value:"4353"
-      },
-      {
-        name:"Increments",
-        value:"2020"
-      },
-      {
-        name:"Arrears",
-        value:"321"
-      }
-    ];
-    this.deductions= [
-      {
-        name:"Stamp",
-        value:"4353"
-      },
-      {
-        name:"No Pay",
-        value:"2020"
-      },
-      {
-        name:"EDCS",
-        value:"321"
-      }
-    ];
+    // this.earnings = [
+    //   {
+    //     name: "Basic Salary",
+    //     value: "4353"
+    //   },
+    //   {
+    //     name: "Increments",
+    //     value: "2020"
+    //   },
+    //   {
+    //     name: "Arrears",
+    //     value: "321"
+    //   }
+    // ];
+    // this.deductions = [
+    //   {
+    //     name: "Stamp",
+    //     value: "4353"
+    //   },
+    //   {
+    //     name: "No Pay",
+    //     value: "2020"
+    //   },
+    //   {
+    //     name: "EDCS",
+    //     value: "321"
+    //   }
+    // ];
   }
 
-  searchSalary(data){
+  searchSalary(data) {
     if (this.searchForm.invalid) return;
+    Swal.showLoading();
+    this.salaryService.getSalaryByUser(data.year, data.month).subscribe(
+      data => {
+        Swal.hideLoading();
+        this.salary = data.salary;
+        this.teacher = data.teacher[0];
+
+        let tempEarnings = [
+          {
+            name: "Pricipal Allowance",
+            value: this.salary.earnings.pricipalAllowance
+          },
+          {
+            name: "Basic Salary",
+            value: this.salary.earnings.basicSalary
+          },
+          {
+            name: "SPE Interim",
+            value: this.salary.earnings.speInterim
+          },
+          {
+            name: "Increments",
+            value: this.salary.earnings.increments
+          },
+          {
+            name: "Arrears",
+            value: this.salary.earnings.arrears
+          },
+          {
+            name: "Festival Advance",
+            value: this.salary.earnings.festivalAdvance
+          },
+          {
+            name: "Adjust All",
+            value: this.salary.earnings.adjustAll
+          },
+          {
+            name: "Allowance",
+            value: this.salary.earnings.allowance
+          },
+          {
+            name: "Co Allowance",
+            value: this.salary.earnings.colAllowance
+          }
+        ];
+
+        let tempDeductions = [
+          {
+            name: "Stamp",
+            value: this.salary.deductions.stamp
+          },
+          {
+            name: "No Pay",
+            value: this.salary.deductions.noPay
+          },
+          {
+            name: "Surcharge",
+            value: this.salary.deductions.surcharge
+          },
+          {
+            name: "W & OP",
+            value: this.salary.deductions.wop
+          },
+          {
+            name: "W & OP Arrears",
+            value: this.salary.deductions.wopArrears
+          },
+          {
+            name: "EDCS",
+            value: this.salary.deductions.edcs
+          },
+          {
+            name: "N. Union",
+            value: this.salary.deductions.nUnion
+          },
+          {
+            name: "Life Insuarance",
+            value: this.salary.deductions.lifeInsurance
+          },
+          {
+            name: "School Deduction",
+            value: this.salary.deductions.schoolDeduction
+          },
+          {
+            name: "House Rent",
+            value: this.salary.deductions.houseRent
+          },
+          {
+            name: "House Loan",
+            value: this.salary.deductions.houseLoan
+          },
+          {
+            name: "Dist. Loan",
+            value: this.salary.deductions.distLoan
+          },
+          {
+            name: "Dist. Int. Arr",
+            value: this.salary.deductions.distIntArr
+          },
+          {
+            name: "FID. DStrs. Lo",
+            value: this.salary.deductions.fidDstrsLo
+          }
+        ];
+
+        this.earnings = tempEarnings.filter(item => {
+          return item.value != null;
+        });
+
+        this.deductions = tempDeductions.filter(item => {
+          return item.value != null;
+        });
+
+        this.name = this.teacher.nameinitials;
+        this.designation = this.teacher.position;
+        this.firstAppointmentDate = this.teacher.firstadmission;
+        this.schoolAppointmentDate = this.teacher.scladmission;
+        this.basicSalary = this.salary.earnings.basicSalary;
+      },
+      error => {
+        this.handleResponseError(error);
+      }
+    );
   }
 
   getError(texttype) {
@@ -78,4 +199,12 @@ export class TeacherSalaryComponent implements OnInit {
       : "";
   }
 
+  handleResponseError(error) {
+    Swal.hideLoading();
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: error.error.error
+    });
+  }
 }
